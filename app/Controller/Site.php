31b612d4cce3ad3a0805779eb4ae27e380937fec
Korkin-Igor <korkinigorttit@gmail.com>
2026-04-23 20:59:40 +0700

    Set up signup

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,19 +1,29 @@
 <?php
 namespace Controller;
 
-use Illuminate\Database\Capsule\Manager as DB;
+use Model\User;
+use Src\Request;
+use Model\Post;
 use Src\View;
 
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
     }
 }
